f0b17ced4818732837777cc1b48b83550eb6a0b1
Chaitanyakumar Bhausaheb Sinare <sinarekumar@gmail.com>
2024-07-13 15:39:23 +0530

    Initial appication code push

--- week-7/src/App.jsx
+++ week-7/src/App.jsx
@@ -4,30 +4,39 @@ import Dashboard from './Components/Dashboard'
 import Landing from './Components/Landing'
 
 function App() {
-  const navigate = useNavigate();
 
   return (
     <>
-      <div>
-        <button onClick={() => {
-          navigate('/')
-        }}>
-          Go to Dashboard
-        </button>
-        <button onClick={() => {
-          navigate('/landing')
-        }}>
-          Go to Landing page
-        </button>
-      </div>
+
       <BrowserRouter>
+        <Appbar />
         <Routes>
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/landing' element={<Landing />} />
         </Routes>
       </BrowserRouter>
+
     </>
   )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={() => {
+        navigate('/dashboard');
+      }}>
+        Go to Dashboard
+      </button>
+      <button onClick={() => {
+        navigate('/landing');
+      }}>
+        Go to Landing page
+      </button>
+    </div>
+  )
+
 }
 
 export default App
